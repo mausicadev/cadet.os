a833154b8a2f79c0735a0b7aa2abfa6d56395d3d
mausica.dev <marius.radulescu@cnmtv.ro>
2026-07-08 01:12:25 +0300

    final.ship

--- legacy_html/js/api/date.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const currentDate = new Date();
-        document.getElementById("month").textContent = currentDate
-          .toLocaleString("default", { month: "long" })
-          .toUpperCase();
-        document.getElementById("year").textContent = currentDate.getFullYear();
-        document.getElementById("day").textContent = currentDate.getDate();
-});
